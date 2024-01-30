8f2ae35d9b596820d17f4214155adc33c379e32f
zabolalo <polya.zab@gmail.com>
2024-01-30 22:27:12 +0100

    Blog Django na zaliczenie

---
+++ ai_web/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article
+# Create your views here.
+def index(response):
+    #return HttpResponse("Pierwszy widok")
+    title_article = "Artykuły"
+    articles = Article.objects.all()
+    options = ["opcja 1", "opcja 2"]
+
+    return render(response, "articles.html",
+                  {
+                      "title_article_view": title_article,
+                      "options": options,
+                      "articles": articles
+                  })
